8000cc3732f31fbc43132ecabb6ed7cc31d74a62
jose restrepo <arturo.restrepo@gmail.com>
2019-07-23 18:42:19 -0500

    debugged part 2 working on saved list

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
+import SavedList from './SavedList' ;
+
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
  
   useEffect(() => {
   
